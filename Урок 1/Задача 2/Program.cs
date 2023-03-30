78118c8e89629787b2fb4cee09949a664ab18426
Dmitriy Manuylov <dmitry.seryozniy@yandex.ru>
2023-03-30 09:44:32 +0300

    Initial commit

---
+++ Урок 1/Задача 2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вводим два числа
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+// Сравниваем их и выводим результат
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else if (a < b)
+{св
+    Console.WriteLine("max = " + b);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
